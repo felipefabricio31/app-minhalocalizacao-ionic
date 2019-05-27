0b04d438227a73d4a0cf59adef52d82b99fc5aed
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-05-27 18:09:46 -0300

    Capturando Latitude e Longitude com a API geolocation.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  latitude: any;
+  longitude: any;
 
+
+  constructor(public geolocation: Geolocation) {
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude
+      this.longitude = resp.coords.longitude
+    }).catch((error) => {
+      console.log('Erro ao capturar localização.', error);
+    });
+  }
 }
